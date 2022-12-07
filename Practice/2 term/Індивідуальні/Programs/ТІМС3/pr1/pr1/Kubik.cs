d1bf8a652998ddf26c1fb23c7fd3d870dd4580cb
Olia Kravets <94845931+helga20@users.noreply.github.com>
2022-12-07 20:37:03 +0200

    Add files via upload

---
+++ Practice/2 term/Індивідуальні/Programs/ТІМС3/pr1/pr1/Kubik.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr1
+{
+    public class Kubik
+    {
+        string name;
+        int colors;
+        double volume;
+        bool normality;
+
+        public Kubik(string name, int colors, double volume, bool normality)
+        {
+            this.name = name;
+            this.colors = colors;
+            this.volume = volume;
+            this.normality = normality;
+        }
+        public Kubik()
+            : this("", 0, 0, false)
+        {
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public int Colors
+        {
+            get
+            {
+                return colors;
+            }
+        }
+        public double Volume
+        {
+            get
+            {
+                return volume;
+            }
+        }
+        public bool Normality
+        {
+            get
+            {
+                return normality;
+            }
+        }
+
+
+    }
+}
---
+++ Practice/2 term/Індивідуальні/Programs/ТІМС3/pr1/pr1/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace pr1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            grid.DataContext = this;
+            kubiks = new ObservableCollection<Kubik>();
+            
+           
+        }
+        private ObservableCollection<Kubik> kubiks;
+        public ObservableCollection<Kubik> Kubiks
+        {
+            get
+            {
+                return kubiks;
+            }
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bool norm = false;
+                if (ry.IsChecked ?? false)
+                {
+                    norm = true;
+                }
+                Kubik kube = new Kubik(name.Text, int.Parse(colors.Text), double.Parse(volume.Text), norm);
+                kubiks.Add(kube);
+                name.Text = "";
+                colors.Text = "";
+                volume.Text = "";
+            }
+            catch 
+            {
+                MessageBox.Show("Incorrect format of input data!" );
+            }
+            
+        }
+
+        
+        private void del_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                kubiks.RemoveAt(lb.SelectedIndex);
+            }
+            catch
+            {
+                MessageBox.Show("No one kubik is selected!");
+            }
+        }
+
+ 
+
+    }
+}
