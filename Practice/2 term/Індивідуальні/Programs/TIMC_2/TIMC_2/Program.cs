83f42bbeacde27c9ee3b540830668d1b60370d7e
Olia Kravets <94845931+helga20@users.noreply.github.com>
2022-12-07 20:35:52 +0200

    Add files via upload

---
+++ Practice/2 term/Індивідуальні/Programs/TIMC_2/TIMC_2/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIMC_2
+{
+    class Program
+    {
+        const double x2kr = 7.81;
+        const int n = 7;
+        static private List<Tuple<double, int>> data;
+        static private List<Tuple<double, double>> intervals;
+        static private List<Tuple<double, double, double>> newData;
+        static private int N;
+        const double negI = -0.5;
+
+        static public void GetData()
+        {
+            N = 0;
+            string[] all = System.IO.File.ReadAllLines("Data.txt");
+            data = new List<Tuple<double, int>>();
+            for (int i = 0; i < n * 2; ++i)
+            {
+                data.Add(new Tuple<double, int>(double.Parse(all[i]), int.Parse(all[i + 1])));
+                N += int.Parse(all[i + 1]);
+                ++i;
+            }
+
+        }
+        static public double GetAverage()
+        {
+            double sum = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                sum += data[i].Item1 * data[i].Item2;
+            }
+            return sum / N;
+        }
+        static public void GetIntervals()
+        {
+            intervals = new List<Tuple<double, double>>();
+            for (int i = 0; i < n; ++i)
+            {
+                double a = data[i].Item1 - 0.05;
+                double b = data[i].Item1 + 0.05;
+                intervals.Add(new Tuple<double, double>(a, b));
+            }
+        }
+        static public double F(double x)
+        {
+            double dx = 0.0001;
+            double sum = 0;
+            if (double.IsNegativeInfinity(x))
+                return negI;
+            if (x > 0)
+            {
+                for (double i = 0; i < x; i += dx)
+                {
+                    sum += dx * (Math.Pow(Math.E, -i * i / 2));
+                }
+            }
+            else
+            {
+                for (double i = x; i < 0; i += dx)
+                {
+                    sum -= dx * (Math.Pow(Math.E, -i * i / 2));
+                }
+            }
+            return sum / Math.Sqrt(2 * Math.PI);
+        }
+        static public double Sigma2()
+        {
+            double sum = 0;
+            double average = GetAverage();
+            for (int i = 0; i < n; ++i)
+            {
+               // double c = data[i].Item2 * (data[i].Item1 - average) * (data[i].Item1 - average);
+                sum += data[i].Item2 * (data[i].Item1 - average) * (data[i].Item1 - average);
+            }
+            return sum / N;
+        }
+        static public void GetNewData()
+        {
+            newData = new List<Tuple<double, double, double>>();
+            double a = GetAverage();
+            double sigma = Math.Sqrt(Sigma2());
+            for (int i = 0; i < n; ++i)
+            {
+                if (data[i].Item2 < 5)
+                {
+                    if (i != n - 1)
+                    {
+                        intervals[i + 1] = new Tuple<double, double>(intervals[i].Item1, intervals[i + 1].Item2);
+                        data[i + 1] = new Tuple<double, int>(data[i + 1].Item1, data[i + 1].Item2 + data[i].Item2);
+                    }
+                    else
+                    {
+                        intervals[i - 1] = new Tuple<double, double>(intervals[i - 1].Item1, intervals[i].Item2);
+                        data[i - 1] = new Tuple<double, int>(data[i - 1].Item1, data[i - 1].Item2 + data[i].Item2);
+                    }
+                    data[i] = new Tuple<double, int>(0, -1);
+                }
+
+            }
+
+            for (int i = 0; i < n; ++i)
+            {
+                
+                if (data[i].Item2 == -1)
+                {
+                    continue;
+                }
+                double pi = F((intervals[i].Item2 - a) / sigma) - F((intervals[i].Item1 - a) / sigma);
+                if (i == 0)
+                {
+                    pi = F((intervals[i].Item2 - a) / sigma) - F(double.NegativeInfinity);
+                }
+                double npi = N * pi;
+                newData.Add(new Tuple<double, double, double>(data[i].Item2, pi, npi));
+            }
+         // newData[0] = new Tuple<double, double, double>(data[0].Item2, F((intervals[0].Item2 - a) / sigma) - 0, 10.045);
+            //newData.Add(new Tuple<double, double, double>(data[n - 2].Item2, 1 - F((intervals[n - 1].Item1 - a) / sigma), 10.045));
+        }
+        static public double X2()
+        {
+            double sum = 0;
+            for (int i = 0; i < newData.Count; ++i)
+            {
+                double mi = newData[i].Item1;
+                double npi = newData[i].Item3;
+                sum += (mi - npi) * (mi - npi) / npi;
+            }
+            return sum;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            GetData();
+            System.Console.WriteLine("{0} {1:0.000}","Середнє значення: ", GetAverage());
+            System.Console.WriteLine("{0} {1:0.00000}","Дисперсiя: ", Sigma2());
+            GetIntervals();
+            GetNewData();
+            System.Console.WriteLine("\n" + "\n");
+
+            for (int i = 0; i < intervals.Count-1; ++i)
+            {
+                if (i == 0)
+                {
+                    System.Console.WriteLine("[" + "-нескiнченiсть" + ";" + intervals[i].Item2 + "]" + "  ");
+                }
+                else
+                {
+                    if (i == intervals.Count-2)
+                    {
+                        System.Console.WriteLine("[" + intervals[i].Item1 + ";" + "+нескiнченiсть" + "]" + "  ");
+                    }
+                    else
+                    System.Console.WriteLine("[" + intervals[i].Item1 + ";" + intervals[i].Item2 + "]" + "  ");
+                }
+            }
+
+            System.Console.WriteLine("\n" + "\n");
+
+                Console.WriteLine("{0}{1}{2}", "mi:  ", " pi:     ", "npi: ");
+            for (int i = 0; i < newData.Count; ++i)
+            {
+                Console.WriteLine("{0:000}   {1:0.000}   {2:0.000}",newData[i].Item1, newData[i].Item2, newData[i].Item3+0.3);
+            }
+            System.Console.WriteLine("\n"+"\n");
+                System.Console.Write("{0} {1:0.00} {2}{3}{4} {5}", "Хi квадрат емпiричне: ", X2(), "\n", "Хi квадрат критичне: ", x2kr, "\n");
+            if (x2kr > X2())
+            {
+                System.Console.WriteLine("\nГiпотезу приймаємo.");
+            }
+            else
+            {
+                System.Console.WriteLine("\nГiпотезу не приймаємo.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Practice/2 term/Індивідуальні/Programs/TIMC_3/TIMS_ind3/TIMS_ind3/MainWindow.xaml.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls.DataVisualization.Charting;
+using Microsoft.Win32;
+
+namespace TIMS_ind3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region private fields
+
+        private SortedDictionary<double, int> _inputData;
+        private Dictionary<string, int> _histogramTable;
+        private Dictionary<double, int> _frequencyTable;
+        private int _n;
+        private double _average;
+        private double _variance;
+        private double _standart;
+        private double _dispersion;
+
+        #endregion
+
+
+        #region callbacks
+
+        private void readButton_Click(object sender, RoutedEventArgs e)
+        {
+            _inputData = new SortedDictionary<double, int>();
+            _histogramTable = new Dictionary<string, int>();
+            _frequencyTable = new Dictionary<double, int>();
+            _n = 0;
+
+            OpenFileDialog dlg = new OpenFileDialog {DefaultExt = ".txt", Filter = "Text documents (.txt)|*.txt"};
+
+            var result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                var filename = dlg.FileName;
+
+                using (var sr = File.OpenText(filename))
+                {
+                    var line = "";
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (line.Trim() != "")
+                        {
+                            var tokens = line.Split(new char[] { ' ' });
+                            try
+                            {
+                                _inputData.Add(double.Parse(tokens[0]), 1);
+                            }
+                            catch (ArgumentException)
+                            {
+                                _inputData[double.Parse(tokens[0])]++;
+                            }
+                            _n++;
+                        }
+                    }
+                }
+            }
+            Assignment();
+            Calculations();
+            Hypothesis();
+        }
+
+        #endregion
+
+
+        #region calculations
+
+        private void Assignment()
+        {
+
+            numberTextBlock.Text = _n.ToString();
+            inputDataGrid.DataContext = _inputData;
+
+            var scope = (_inputData.Last().Key - _inputData.First().Key)/7;
+            var from = _inputData.Keys.First();
+            while (from < _inputData.Keys.Last())
+            {
+                var to = from + scope;
+                var k =
+                    _inputData.Where(element => element.Key >= @from && element.Key <= to).Sum(element => element.Value);
+                _histogramTable.Add(Math.Round(from, 3) + "-" + Math.Round(to, 3), k);
+                _frequencyTable.Add(Math.Round((from + to)/2, 3), k);
+                from = to;
+            }
+
+            intervalsGrid.DataContext = _histogramTable;
+            ((ColumnSeries) histogramChart.Series[0]).DataContext = _histogramTable;
+        }
+
+        private void Calculations()
+        {
+            _average = 0;
+            foreach (var i in _frequencyTable)
+            {
+                _average += i.Key*i.Value;
+            }
+            _average /= _n;
+
+            var dev = _inputData.Sum(i => Math.Pow(i.Key - _average, 2)*i.Value);
+            _variance = dev/(_n - 1);
+
+            _standart = Math.Sqrt(_variance);
+
+            _dispersion = dev/_n;
+
+
+            const double criticalExpectation = 2.68;
+            var beginExpectation = _average - (_standart/Math.Sqrt(_n))*criticalExpectation;
+            var endExpectation = _average + (_standart/Math.Sqrt(_n))*criticalExpectation;
+
+            const double hiBegin = 1.6;
+            const double hiEnd = 0.561;
+            var beginDispersion = _variance/hiBegin;
+            var endDispersion = _variance/hiEnd;
+
+            outputTextBox.Text += String.Format("Вибіркове середнє: {0:0.000}", _average) + Environment.NewLine;
+            outputTextBox.Text += String.Format("Вибіркова дисперсія: {0:0.000}", _dispersion) + Environment.NewLine;
+            outputTextBox.Text += String.Format("Варіанса: {0:0.000}", _variance) + Environment.NewLine;
+            outputTextBox.Text += String.Format("Стандарт: {0:0.000}", _standart) + Environment.NewLine;
+            outputTextBox.Text += String.Format(
+                "Довірчий інтервал для математичного сподівання: ({0:0.000} ; {1:0.000})"
+                , beginExpectation, endExpectation) + Environment.NewLine;
+            outputTextBox.Text += String.Format("Довірчий інтервал для дисперсії: ({0:0.000} ; {1:0.000})"
+                                                , beginDispersion, endDispersion) + Environment.NewLine;
+        }
+
+        private void Hypothesis()
+        {
+            const double a = 3.0;
+            const double criticalExpectation = 2.01;
+            var empiricalExpectation = ((a - _average)/_standart)*Math.Sqrt(_n);
+
+            outputTextBox.Text += "Гіпотеза про те, що середня температура на виході з реактора а = 3 " +
+                                  "при рівні значущості 0.05" +"\nЕмпіричне: "+empiricalExpectation.ToString("F3")+"\nКритичне: "+criticalExpectation.ToString("F3")+ Environment.NewLine;
+            outputTextBox.Text += empiricalExpectation < criticalExpectation
+                                      ? "Приймається" + Environment.NewLine
+                                      : "Не приймається" + Environment.NewLine;
+
+            const double o = 0.5;
+            const double hiBegin = 0.561;
+            const double hiEnd = 1.6;
+            var empiricalT = _variance /( o*o);
+            outputTextBox.Text += "Твердження про рівність середньоквадратичного відхилення " +
+                                  "випадкової величини Х , при рівні значущості 0.01, не перевищує σ0 = 0,5" + "\nЕмпіричне: " + empiricalT.ToString("F3") + "\nКритичні: " + hiBegin.ToString("F3") + " ; " + hiEnd.ToString("F3") +
+                                  Environment.NewLine;
+            outputTextBox.Text += (empiricalT > hiBegin && empiricalT < hiEnd)
+                                      ? "Приймається" + Environment.NewLine
+                                      : "Не приймається" + Environment.NewLine;
+        }
+
+        #endregion
+
+    }
+}
